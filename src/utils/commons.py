cc5de291d408f843b58d0b613817f2298cad01ae
VulcanoAhab <vulcano.ahab@gmail.com>
2018-08-04 00:12:46 -0300

    Improved building loop

--- src/utils/commons.py
+++ src/utils/commons.py
@@ -5,7 +5,6 @@ def iterModules(baseObj, dotString):
     dotList=dotString.split(".")
     if not dotList:
         raise Exception("[-] Empty dotString")
-    print(dotList)
     obj=getattr(baseObj,dotList[0])
     for objName in  dotList[1:]:
         obj=getattr(obj, objName)
--- topologies/tempest.py
+++ topologies/tempest.py
@@ -14,19 +14,31 @@ _JOB_CONF="topologies/job_config.yaml"
 if os.getenv("STORM_DEBUG"):
     _JOB_CONF="topologies/dev_config.yaml"
 
+TYPE_SPOUT="spout"
+TYPE_BOLD="bolt"
 
 class Tempest(Topology):
     """
     """
     job=Job(_JOB_CONF)
     job.load()
+
+    last_type=None
+    tempestBolt=None
+    tempestSpout=None
+
     for config in job.components:
-        if config["type"] == "spout":
+
+        if config["type"] == TYPE_SPOUT:
+            print("[+] ADDING SPOUT:{}".format(config["module"]))
             #module=importlib.import_module(config["module"], package=spouts)
             spout_module="spouts.{}".format(config["module"])
             module=importlib.import_module(spout_module)
-            streamObj=getattr(module,  config["class"]).spec()
-        elif config["type"] == "bolts":
+            tempestSpout=getattr(module,  config["class"]).spec()
+            last_type=config["type"]
+
+        elif config["type"] ==TYPE_BOLD:
+            print("[+] ADDING BOLT:{}".format(config["module"]))
             groupingMode=config.get(config["grouping"], "SHUFFLE")
             if groupingMode == "fields":
                 grouping_field=config["grouping_field"]
@@ -35,6 +47,12 @@ class Tempest(Topology):
                 grouping=getattr(Grouping, groupingMode)
             bolt_module="bolts.{}".format(config["module"])
             module=importlib.import_module(bolt_module)
-            streamObj=iterModules(module,  config["class"]).spec(
-                                            inputs={streamObj:grouping},
+            if last_type == TYPE_SPOUT:
+                insDict={tempestSpout:grouping}
+            else:
+                insDict={tempestBolt:grouping}
+            tempestBolt=iterModules(module, config["class"]).spec(
+                                            inputs=insDict,
                                             par=config.get("par", 1))
+            if not tempestBolt:break
+            last_type=config["type"]
